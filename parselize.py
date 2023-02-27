2603b44d3472bcac6f28fc05e8036956b7e754b9
Karan Ramanathan <karan_ramanathan@clearpoint.co.nz>
2023-02-28 03:15:22 +0530

    Add functionality to return biggest dimension of a parcel

--- parselize.py
+++ parselize.py
@@ -6,6 +6,9 @@ class Parcel:
         self.breadth = breadth
         self.height = height
 
+    def get_biggest_dimension_of_parcel(self, parcel: tuple) -> float:
+        return max(parcel)
+
 def get_input_from_user():
     print("Please enter parcel dimensions in the form of length, breadth, height.")
     print("To enter multiple parcel dimensions, please enter the next parcel dimensions seperated by a space. For example - 1,2,3 4,5,6.")
@@ -16,15 +19,25 @@ def parse_user_input(input):
     try:
         # split by space to check for multiple parcels
         parcel_dimension = input.split(" ")
-        # returns a list of parcel dimension tuples
+        # returns a list of parcel dimension tuples for eg - [(1,2,3), (4,5,6)]
         return [tuple(int(x) for x in i.split(",")) for i in parcel_dimension]
     except ValueError:
         print("Please enter only numbers as parcel dimensions!")
+        exit()
 
 def main():
     input = get_input_from_user()
-    parcel_dimension = parse_user_input(input)
+    parcel_dimensions = parse_user_input(input)
 
+    for parcel_dimension in parcel_dimensions:
+        try:
+            parcel = Parcel(*parcel_dimension)
+        except TypeError:
+            print("Input has not been entered in valid format!")
+            print("For multiple parcels, please enter parcel dimensions separated by a space!")
+            exit()
+
+        max_dimension = parcel.get_biggest_dimension_of_parcel(parcel_dimension)
 
 if __name__ == "__main__":
     main()
