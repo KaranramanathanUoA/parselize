d8dccaf03a18cb14174c1392e7816bd07ec3baaf
Karan Ramanathan <karan_ramanathan@clearpoint.co.nz>
2023-02-28 03:10:11 +0530

    Add more tests to validate parsing of user inputs

--- parselize_test.py
+++ parselize_test.py
@@ -14,6 +14,17 @@ class ParselizeTests(unittest.TestCase):
         input = "1,2,4"
         self.assertEqual(parse_user_input(input), [(1,2,4)])
 
+    def test_parcel_dimensions_are_correctly_parsed_with_multiple_dimensions(self):
+        input = "12,15,85 6,45,100"
+        self.assertEqual(parse_user_input(input), [(12,15,85), (6,45,100)])
+
+    def test_exception_is_thrown_when_parsing_invalid_inputs(self):
+        input = "a,b,c"
+        with self.assertRaises(SystemExit):
+            parse_user_input(input)
+
+    
+
 if __name__ == "__main__":
     unittest.main()
         
